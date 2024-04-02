b01707ad8aff02c1081637ffc7a8f78f54535a05
geangd7 <131774751+geangd7@users.noreply.github.com>
2024-04-02 08:35:38 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,5 @@
+// Declaração da variável constante botao para representar os elementos que devem ser lidos
+const botao = document.querySelectorAll('.botao');
+
+// Verificar o progresso inserindo console.log(botao)
+console.log(botao);
